2d6fbde2eca30dfc553734b5baacf2e00480f704
Vitor Raul Bim <vbim02@prefeitura.bebedouro>
2020-09-28 10:57:39 -0300

    Correção do projeto

--- Clientes.py
+++ Clientes.py
@@ -1,7 +1,5 @@
 import FunctionsData
 
-#comentário
-#comentário 2
 
 def initializer():
     answerNewClient = int(input('Digite [1] - Para logar com um perfil existente e [2] - Para criar um novo:  '))
